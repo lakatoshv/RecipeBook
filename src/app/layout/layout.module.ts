ebcb254674083afcf84fcf946c53bb81e0f48dc6
Vitaliy Lakatosh <vlakatosh@netfully.com>
2019-07-05 16:49:41 +0300

    Changed layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,14 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
-import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
+import { MenuComponent } from '../menu/menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { SpecialRecipesComponent } from '../recipes/special-recipes/special-recipes.component';
+import { ReviewsListComponent } from '../reviews/reviews-list/reviews-list.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SubscribeFormComponent } from '../subscribe/subscribe-form/subscribe-form.component';
 
 @NgModule({
-  declarations: [HeaderComponent, LayoutComponent],
+  declarations: [
+    HeaderComponent, 
+    LayoutComponent, 
+    MenuComponent, 
+    AboutComponent, 
+    SpecialRecipesComponent,
+    ReviewsListComponent,
+    FooterComponent,
+    SubscribeFormComponent
+  ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
   ]
 })
 export class LayoutModule { }

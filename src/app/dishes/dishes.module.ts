6f5d0a41662ec17dbc0c7c07f328d949c1a7a3fa
Vitaliy Lakatosh <vlakatosh@netfully.com>
2019-08-20 19:13:50 +0300

    Added add dish form

--- src/app/dishes/dishes.module.ts
+++ src/app/dishes/dishes.module.ts
@@ -4,14 +4,19 @@ import { DishesRoutingModule } from './dishes-routing.module';
 import { DishesListComponent } from './dishes-list/dishes-list.component';
 import { MenuModule } from '../menu/menu.module';
 import { DishComponent } from './dish/dish.component';
+import { AddDishComponent } from './add-dish/add-dish.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditorModule } from '@tinymce/tinymce-angular';
+
 
 @NgModule({
-  declarations: [DishesListComponent, DishComponent ],
+  declarations: [DishesListComponent, DishComponent, AddDishComponent ],
   imports: [
     CommonModule,
     DishesRoutingModule,
-    MenuModule
-    
+    MenuModule,
+    ReactiveFormsModule,
+    EditorModule
   ]
 })
 export class DishesModule { }
